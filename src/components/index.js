5a164f7a8679cc26a3fb6e3c7d52941590a5161b
duchau <duchaunee@gmail.com>
2024-04-06 04:34:05 +0700

    deply error path, create bug in components/index.js to fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Header } from "./header/Header.jsx";
-export { default as Footer } from "./footer/Footer.jsx";
 export { default as ProductItem } from "./productItem/ProductItem";
+export { default as Header } from "./header/Header";
+export { default as Footer } from "./footer/Footer";
 export { default as ButtonPrimary } from "./button/ButtonPrimary";
 export { default as Card } from "./card/Card";
 export { default as Auth } from "./auth/Auth";
